a6a5ac0d54acdccfbf77c5c86cc80d14ae84ee7f
Rushikesh Nikam <rushidnikam00@gmail.com>
2024-08-24 23:32:16 +0530

    stree_dine_new

---
+++ Intern/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Left_sec from './Components/Left_sections/Left_sec'
+import Router from './Components/route/Router'
+
+function App() {
+  
+
+  return (
+    <>
+    <Router/>
+    </>
+  )
+}
+
+export default App
